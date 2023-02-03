e333417a13f4ffdfebb3b00d202c4ff78036afb6
Shmithy16 <124367787+Shmithy16@users.noreply.github.com>
2023-02-03 13:54:50 +0000

    Add files via upload
    First upload P5 draw sketch

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+  background(120);
+}
+
+function draw() {
+  
+
+
+}
+
+function mouseDragged(){
+  noStroke()
+  fill(200, 120, 80)
+  circle(mouseX, mouseY, 20)
+
+}
